c51cf87a166b260b8ed891b69a67bb1426acfffb
HurleyKane <chmtk@outlook.com>
2024-10-27 22:55:20 +0800

    fixed bug

--- geodesy/__init__.py
+++ geodesy/__init__.py
@@ -1,6 +1,4 @@
 from geodesy.algorithm import *
 from geodesy.dataframe import *
 from geodesy.plotting import *
-from geodesy.earthquake import *
-from geodesy.io import *
 from geodesy import config
